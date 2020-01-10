0b0e8611f98d824ddec7406a67aaf0a2302b3a30
Jesse Martinez <jezze.04@gmail.com>
2020-01-10 03:28:15 -0600

    added a header to portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,64 @@
 import React, {PureComponent} from 'react';
-
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import {Link} from 'react-router-dom';
 import Project from '../components/Project';
 import {WebProjects} from '../assets/data/projectData';
 
+function typewriterAnimation(steps, delay, count){
+  let inf = (count)?count:'infinite';
+  let delayType = delay + .5
+  return(
+    {
+      translateZ: '0',
+      fontSize: '3.5vmax',
+      animation: 'typewriter '+(1)+'s steps('+steps+') '+delayType+'s 1 normal both,'+
+      'blinkTextCursor 500ms steps('+steps+') '+delay+'s '+inf+' normal'
+    }
+  );
+}
+
 class Portfolio extends PureComponent {
+
+
+  renderHeader(){
+    return(
+      <div style={{
+        textAlign: 'center',
+        paddingTop: '25vh',
+        height: '75vh',
+        background: 'radial-gradient(circle, rgba(148, 0, 211,.25), black 60%)',
+        backgroundPosition: 'center',
+        animation: 'pulse 5s ease infinite'
+      }}>
+        <Typography className="anim-typewriter"
+          style={typewriterAnimation(135, 0, 3)}>
+          PROJECT:
+        </Typography><br />
+        <Typography className="anim-typewriter"
+          style={typewriterAnimation(135, 1.5, 3)}>
+          PORTFOLIO
+        </Typography><br /><br />
+        <Typography className="anim-typewriter"
+          style={typewriterAnimation(135, 3, 0)}>
+          Jesus G Martinez
+        </Typography><br /><br /><br />
+        <Button component={Link} to="/resume" classes={{root: 'resume-link'}}>
+          To My Resume
+        </Button>
+        <Typography style={{position: 'absolute', bottom: '30px', width: '100%'}}>
+          Scroll to continue
+        </Typography>
+      </div>
+    );
+  }
+
+
   render(){
     return(
       <div style={{background: 'black', color: 'gainsboro'}}>
-        <div>
+          {this.renderHeader()}
+          <div>
           {
             WebProjects.map( (data, index) => (
               <Project
