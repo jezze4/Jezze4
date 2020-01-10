335f29c3e23e2f6e55e3ee9d16a70b16764922ea
Jesse Martinez <jezze.04@gmail.com>
2020-01-09 18:27:25 -0600

    added remaining images for the other websites. added buttons tottons to change between simulated pages. looks amazing! added some resopnsiveness

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import SRL_D1 from '../imgs/srl-screen1.png'
-import SRL_D2 from '../imgs/srl-screen2.png'
-import SRL_D3 from '../imgs/srl-screen3.png'
-import SRL_M1 from '../imgs/srl-mobile1_sm.jpg'
-import SRL_M2 from '../imgs/srl-mobile2_sm.jpg'
-import SRL_M3 from '../imgs/srl-mobile3_sm.jpg'
+import SRL_D1 from '../imgs/srl-home.png'
+import SRL_D2 from '../imgs/srl-appts.png'
+import SRL_D3 from '../imgs/srl-service.png'
+import SRL_M1 from '../imgs/srl-home_m.jpg'
+import SRL_M2 from '../imgs/srl-appts_m.jpg'
+import SRL_M3 from '../imgs/srl-service_m.jpg'
+
+import LDO_D1 from '../imgs/ldo-home.png';
+import LDO_D2 from '../imgs/ldo-about.png';
+import LDO_D3 from '../imgs/ldo-apply.png';
+import LDO_M1 from '../imgs/ldo-home_m.jpg';
+import LDO_M2 from '../imgs/ldo-about_m.jpg';
+import LDO_M3 from '../imgs/ldo-apply_m.jpg';
+
+import VOTE_D1 from '../imgs/vote-home.png';
+import VOTE_D2 from '../imgs/vote-states.png';
+import VOTE_D3 from '../imgs/vote-rep.png';
+import VOTE_D4 from '../imgs/vote-map.png';
+import VOTE_M1 from '../imgs/vote-home_m.jpg';
+import VOTE_M2 from '../imgs/vote-states_m.jpg';
+import VOTE_M3 from '../imgs/vote-rep_m.jpg';
+import VOTE_M4 from '../imgs/vote-map_m.jpg';
 
 const WebProjects = [
   {
@@ -17,6 +33,7 @@ const WebProjects = [
     database: 'Firestore',
     imagesDesktop: [SRL_D1, SRL_D2, SRL_D3],
     imagesMobile: [SRL_M1, SRL_M2, SRL_M3],
+    imagesTitles: ['Home','Apptointments','Services'],
     imageDisplay: null,
     link: 'https://sportsrecoverylounge.com'
   },
@@ -28,8 +45,9 @@ const WebProjects = [
     frontEnd: 'ReactJS',
     backEnd: 'Firebase',
     database: 'Firestore',
-    imagesDesktop: [null, null, null],
-    imagesMobile: [null, null, null],
+    imagesDesktop: [LDO_D1, LDO_D2, LDO_D3],
+    imagesMobile: [LDO_M1, LDO_M2, LDO_M3],
+    imagesTitles: ['Home','About','Submitting Application'],
     imageDisplay: null,
     link: 'https://ldo-staffing.web.app'
   },
@@ -41,8 +59,9 @@ const WebProjects = [
     frontEnd: 'ReactJS',
     backEnd: 'NodeJS + ExpressJS',
     database: 'AWS, PostgreSQL',
-    imagesDesktop: [null, null, null],
-    imagesMobile: [null, null, null],
+    imagesDesktop: [VOTE_D1, VOTE_D2, VOTE_D3, VOTE_D4],
+    imagesMobile: [VOTE_M1, VOTE_M2, VOTE_M3, VOTE_M4],
+    imagesTitles: ['Home','States Directory','Representative', 'Interactive Map'],
     imageDisplay: null,
     link: 'https://thewisevote.com'
   },
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 import React, {PureComponent} from 'react';
 import {
-  Typography, Grid
+  Typography, Grid, Button
 } from '@material-ui/core';
 import SwipeableViews from 'react-swipeable-views';
 
@@ -12,22 +12,57 @@ import '../assets/css/Project.css';
 
 class Project extends PureComponent {
 
+  state = {
+    index: 0,
+  }
+
 
   renderDesktopImages(images){
+    const {index} = this.state;
     return(
       <div>
-        <img src={DesktopTemplate} className='desktop-template' alt='desktop-template' />
-        <img src={images[0]} alt="screenshot1" className='desktop-img-1'/>
+        <img src={DesktopTemplate} className='desktop-template no-drag' alt='desktop-template' />
+        <SwipeableViews index={index} className="desktop-img-container">
+          {images.map((image, imgIndex) =>
+            <img src={image} key={`screenshot-${imgIndex}`}
+              alt={`screenshot-${imgIndex}`} className='desktop-img-1' />
+          )}
+        </SwipeableViews>
       </div>
     );
   }
 
   renderMobileImages(images){
+    const {index} = this.state;
     return(
       <div>
-        <img src={MobileTemplate} className='mobile-template' alt='mobile-template'/>
-        <img src={images[0]} alt="screenshot1" className="mobile-img-1"/>
+        <img src={MobileTemplate} className='mobile-template no-drag' alt='mobile-template'/>
+        <SwipeableViews index={index} className="mobile-img-container">
+          {images.map((image, imgIndex) =>
+            <img src={image} key={`screenshot-${imgIndex}`}
+              alt={`screenshot-${imgIndex}`} className='mobile-img-1' />
+          )}
+          {/* <img src={images[0]} alt="screenshot1" className="mobile-img-1"/>
+          <img src={images[1]} alt="screenshot1" className="mobile-img-1"/>
+          <img src={images[2]} alt="screenshot1" className="mobile-img-1"/> */}
+        </SwipeableViews>
       </div>
+    );
+  }
+
+  renderTitles(titles){
+    return(
+      <Grid container direction="column" justify="space-evenly" alignItems="stretch"
+        style={{height: '100%'}}>
+        {titles.map( (title, index) =>
+          <Grid item key={`imageTitle-${index}`}>
+            <Button fullWidth variant="outlined"
+              onClick={()=>this.setState({index})}>
+              {title}
+            </Button>
+          </Grid>
+        )}
+      </Grid>
     );
   }
 
@@ -35,7 +70,7 @@ class Project extends PureComponent {
     const {project} = this.props;
     return(
       <div className="project-root">
-        {/* <Typography variant="h3">{project.title}</Typography> */}
+        <Typography variant="h3">{project.title}</Typography>
         <Typography component="p">{project.description}</Typography>
         <Typography component="p">Design Style: {project.type}</Typography>
         <Typography component="p">
@@ -45,10 +80,13 @@ class Project extends PureComponent {
           Visit Site
         </Typography>
         <Grid container direction="row" justify="space-around">
-          <Grid item>
+          <Grid item sm={12} md={5}>
             {this.renderDesktopImages(project.imagesDesktop)}
           </Grid>
-          <Grid item>
+          <Grid item sm={4} md={2}>
+            {this.renderTitles(project.imagesTitles)}
+          </Grid>
+          <Grid item sm={12} md={3}>
             {this.renderMobileImages(project.imagesMobile)}
           </Grid>
         </Grid>
