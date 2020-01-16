8183bc1c379a3fc7d0a186f2be365808c3520fa1
Jesse Martinez <jezze.04@gmail.com>
2020-01-15 23:34:02 -0600

    temporarily removed some parallax features until bugs are fre fixed

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,6 +16,8 @@ class Project extends PureComponent {
     index: 0,
     componentOffset: 0,
     offset: 0,
+    viewHeight: window.innerHeight,
+    ref: null,
   }
 
   componentDidMount() {
@@ -28,12 +30,23 @@ class Project extends PureComponent {
     window.removeEventListener('scroll', this.parallaxHeader);
   }
 
+  componentDidUpdate() {
+    const {viewHeight, ref, componentOffset} = this.state;
+    if(ref){
+      if(viewHeight !== window.innerHeight){
+        let diff = componentOffset + (window.innerHeight - viewHeight);
+        this.setState({viewHeight: window.innerHeight})
+        this.setState({componentOffset: diff})
+      }
+    }
+  }
+
   parallaxHeader = () => {
     const {componentOffset} = this.state;
     let min = componentOffset - window.innerHeight;
     let max = componentOffset + window.innerHeight;
     if(window.pageYOffset >= min && window.pageYOffset <= max){
-      this.setState({offset: Math.abs(window.pageYOffset - componentOffset)});
+      this.setState({offset: -(window.pageYOffset - componentOffset)});
     }
   }
 
@@ -87,14 +100,26 @@ class Project extends PureComponent {
     );
   }
 
+  getDimensions = element => {
+    if(element){
+      this.setState({ref: element});
+      this.setState({componentOffset: element.getBoundingClientRect().y})
+    }
+  }
+
   render(){
     const {project} = this.props;
-    const {offset} = this.state;
-    let opacityValue = (window.innerHeight - (offset*1.7)) / window.innerHeight;
-    let scaleValue = 5 - ((window.innerHeight - offset) / window.innerHeight * 5);
+    const {offset, viewHeight} = this.state;
+    let opacityValue = (viewHeight - (offset*1.7)) / viewHeight;
+    let blurValue = 5 - ((viewHeight - offset) / viewHeight * 5);
     return(
       <div style={{height: '250vh'}}>
-        <div className="project-intro" style={{opacity: opacityValue, filter: `blur(${scaleValue}px)`}}>
+        <div
+          className="project-intro"
+          style={{opacity: opacityValue, filter: `blur(${blurValue}px)`}}
+          ref={this.getDimensions}
+          >
+          {/* <Typography variant="h3" style={{padding: '32px 0'}}>{offset.toFixed(2)}</Typography> */}
           <Typography variant="h3" style={{padding: '32px 0'}}>{project.title}</Typography>
           <Typography component="p" className="project-desc" style={{paddingBottom: '24px'}}>
             {project.description}
